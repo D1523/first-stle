15abe9810f0b7d0d74d361729d7ad8feabf56c05
Erkan_filiz <erkanfiliz1715@gmail.com>
2023-07-23 02:48:30 +0200

    React Module Style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/card/Card";
+import data from "./data";
 
 function App() {
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Languages</h1>
+{/*iterasyon*/}
+{data.map(({id, language, img, btnName })=>(
+
+      <Card key={id} lang={language} img={img} btn={btnName}/>
+))}
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,15 @@
+import  Buton  from "../buton/Buton"
+import CardStyle from "./Card.module.css"
+
+const Card = ({lang, btn, img}) => {
+  return (
+    <div>
+      <h1 className={CardStyle["title"]}>{lang}</h1>
+      <img className={CardStyle["images"]} src={img} alt="img"/>
+           
+           <Buton btn={btn}/>
+    </div>
+  )
+}
+
+export default Card
